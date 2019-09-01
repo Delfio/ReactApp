e64782df914654402fe304b38561f08f64a53b49
Delfio <delfio_eu@hotmail.com>
2019-09-01 15:05:07 -0400

    Initial commit

---
+++ babel.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  presets: [
+    "@babel/preset-env", // Convete toda a linguagem js para o navegador entender
+    "@babel/preset-react" // Convete todas as coisas do react para o navegador entender
+  ],
+};
